ed0b93786ba4de1538ceeacc71c20595d69100f5
Chris Chung <chrisjhchung@gmail.com>
2024-01-20 18:00:13 +1100

    chore: add missing pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,8 @@ import NavigationBar from "./components/NavigationBar/NavigationBar";
 import Blog from "./pages/Blog/Blog";
 import React from "react";
 import BlogPost from "./pages/BlogPost/BlogPost";
+import About from "./pages/About/About";
+import Projects from "./pages/Projects/Projects";
 
 function App() {
   return (
@@ -26,6 +28,8 @@ function App() {
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog/:slug" element={<BlogPost />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </KBarProvider>
     </Router>
---
+++ frontend/src/pages/About/About.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./About.module.css";
+
+const About = () => {
+  return (
+    <div className={styles.container}>
+      <h1>About</h1>
+      <p>Life story goes here</p>
+    </div>
+  );
+};
+
+export default About;
