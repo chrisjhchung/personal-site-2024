7e05b46744325cbc9d0c51de09eb41cb14242bd3
Chris Chung <chrisjhchung@gmail.com>
2024-08-25 10:56:25 +1000

    feat: recreate home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
+import GradientBackground from "../components/gradientBackground/gradientBackground";
 
 export default function Home() {
-  return <div>Chris Chung</div>;
+  return (
+    <div className={styles.container}>
+      <GradientBackground />
+      <h1 className={styles.header}>Chris Chung.</h1>
+    </div>
+  );
 }
