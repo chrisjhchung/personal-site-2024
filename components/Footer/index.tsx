805b2c1c1e4b483475104519aee4395e0196dd1d
Chris Chung <chrisjhchung@gmail.com>
2024-09-15 17:08:07 +1000

    chore: clean up

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,47 +1,36 @@
-import { Twitter, Instagram, GitHub, Linkedin } from "react-feather";
-import siteConfig from "@/data/siteConfig";
-import { cx } from "@/lib/utils";
-import { ThemeSelect } from "@/components/ThemeSelect";
-import styles from "./index.module.scss";
-
-const iconProps = { className: "w-4 h-4" };
+import siteConfig from '@/data/siteConfig';
+import styles from './index.module.scss';
 
 export const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      {siteConfig.social ? (
+      {siteConfig.social && (
         <ul className="flex justify-center space-x-4">
-          {Object.entries(siteConfig.social).map(([key, href]) => {
-            return (
-              <li key={key}>
-                <a
-                  href={href}
-                  className={cx(
-                    "w-8 h-8 grid place-items-center rounded-md",
-                    "bg-gray-900 text-gray-50",
-                    "dark:bg-gray-800 dark:text-gray-50"
-                  )}
-                  title={key}
+          {Object.entries(siteConfig.social).map(([key, href]) => (
+            <li key={key}>
+              <a
+                href={href}
+                className={`${styles.socialIcon} ${styles[key]}`}
+                title={key}
+              >
+                <svg
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <svg
-                    width="12"
-                    height="12"
-                    viewBox="0 0 12 12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
-                      fill="currentColor"
-                    ></path>
-                  </svg>
-                  {key}
-                </a>
-              </li>
-            );
-          })}
+                  <path
+                    d="M2.07102 11.3494L0.963068 10.2415L9.2017 1.98864H2.83807L2.85227 0.454545H11.8438V9.46023H10.2955L10.3097 3.09659L2.07102 11.3494Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+                {key}
+              </a>
+            </li>
+          ))}
         </ul>
-      ) : null}
+      )}
       <p>© 2024 Chris Chung</p>
     </footer>
   );
